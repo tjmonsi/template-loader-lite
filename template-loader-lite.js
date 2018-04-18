168795625e14dae9ef43912e789a03d9644205ef
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-04-18 05:14:46 +0000

    Fixed #3

---
+++ template-loader-lite.js
@@ -0,0 +1,65 @@
+/**
+ * # template-loader-lite
+ *
+ * `<template-loader-lite>`: A template loading device
+ *
+ * @extends {HTMLElement}
+ * @customElement
+*/
+
+class TemplateLoaderLite extends window.HTMLElement {
+  static get is () {
+    return 'template-loader-lite';
+  }
+  
+  static get observedAttributes () {
+    return ['pattern'];
+  }
+  
+  constructor () {
+    super();
+    this.__data = {};
+    this.__patternInitialized = false;
+  }
+  
+  connectedCallback () {
+    this.__patternInitialized = true;
+    this._patternChanged(this.pattern || this.getAttribute('pattern'));
+  }
+  
+  set pattern (pattern) {
+    this.__data.pattern = pattern;
+    if (this.__patternInitialized) {
+      this._patternChanged(pattern);
+    }
+  }
+
+  get pattern () {
+    return this.__data.pattern;
+  }
+  
+  _patternChanged (pattern) {
+    const template = this.querySelector(`template[pattern=${pattern}]`);
+    if (template) {
+      this.template = template;
+      Promise.resolve().then(() => {
+        this.dispatchEvent(new window.CustomEvent('template-change', { detail: this.template }));
+      });
+    } else {
+      console.warn(`There's no template available for pattern: `, pattern);
+    }
+  }
+  
+  attributeChangedCallback (name, oldValue, newValue) {
+    if (name === 'pattern' && this.__data.pattern !== newValue) {
+      this.__data.pattern = newValue;
+      this._patternChanged(newValue);
+    }
+  }
+}
+
+if (!window.customElements.get(TemplateLoaderLite.is)) {
+  window.customElements.define(TemplateLoaderLite.is, TemplateLoaderLite);
+} else {
+  console.warn(`${TemplateLoaderLite.is} is already defined somewhere. Please check your code.`);
+}
